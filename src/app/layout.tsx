74c8d9e8d27e525f8c2b1435865ff98229bf206f
londonclean01-site <londonclean01@gmail.com>
2026-04-04 16:40:11 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,11 +30,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-        />
+  <Script
+    id="gtm-script"
+    strategy="afterInteractive"
+    src="https://www.googletagmanager.com/gtm.js?id=GTM-KR4DSTKH"
+  />
       </head>
       <body
         className={cn(
@@ -42,6 +42,14 @@ export default function RootLayout({
           inter.variable
         )}
       >
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-KR4DSTKH"
+      height="0"
+      width="0"
+      style={{ display: "none", visibility: "hidden" }}
+    />
+  </noscript>
         <LocalBusinessSchema />
         <Header />
         <main className="flex-1 min-h-screen pt-20">
